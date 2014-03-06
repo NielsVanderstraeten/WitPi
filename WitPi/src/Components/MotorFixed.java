4f9a846af25280139353f60dcd063ebe30c26a03
JeroenCo <jeroen.compagnie@student.kuleuven.be>
2014-03-06 15:03:05 +0100

    zie vorige..

---
+++ WitPi/src/Components/MotorFixed.java
@@ -0,0 +1,85 @@
+package Components;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+//import com.pi4j.io.gpio.RaspiPin;
+
+public class MotorFixed {
+	protected final static GpioController gpio = GpioFactory.getInstance();
+	// twee pinnen: 1 om voorwaarts te draaien, 1 om achterwaarts te draaien
+	protected final GpioPinDigitalOutput trigPinForward;
+	protected final GpioPinDigitalOutput trigPinBackward;
+	protected boolean forwardOn = false;
+	protected boolean backwardOn = false;
+
+	public MotorFixed(Pin trigPinForward, Pin trigPinBackward){
+		this.trigPinForward = gpio.provisionDigitalOutputPin(trigPinForward);
+		this.trigPinBackward = gpio.provisionDigitalOutputPin(trigPinBackward);
+		this.trigPinForward.low();
+		this.trigPinBackward.low();
+	}		
+	
+	public void triggerForwardOn(){
+		if(backwardOn){
+			trigPinBackward.low();
+			backwardOn = false;
+		}
+		trigPinForward.high();
+                forwardOn = true;
+	}
+	
+	public void triggerForwardOff(){
+		trigPinForward.low();
+		forwardOn = false;
+	}
+
+	public void triggerBackwardOn(){
+		if(forwardOn){
+			trigPinForward.low();
+			forwardOn = false;
+		}
+		trigPinBackward.high();
+		backwardOn = true;
+	}
+
+	public void triggerBackwardOff(){
+		trigPinBackward.low();
+		backwardOn = false;
+	}
+
+	/*public static void main( String[] args ) {
+		//Motor1
+		Pin forw = RaspiPin.GPIO_05;
+		Pin back = RaspiPin.GPIO_07;
+		//Motor2
+		//Pin forw = RaspiPin.GPIO_04;
+		//Pin back = RaspiPin.GPIO_00;
+		//Motor3
+		//Pin forw = RaspiPin.GPIO_13;
+		//Pin back = RaspiPin.GPIO_11;
+		//Motor4
+		//Pin forw = RaspiPin.GPIO_12;
+		//Pin back = RaspiPin.GPIO_14;
+		try{
+			System.out.println("Hello");
+			MotorFixed fixedMotor = new MotorFixed(forw, back);
+			fixedMotor.triggerForwardOn();
+			System.out.println("Forward spin on");
+			Thread.sleep(3000);
+			fixedMotor.triggerForwardOff();
+			System.out.println("Forward spin off");
+			Thread.sleep(3000);
+			fixedMotor.triggerBackwardOn();
+			System.out.println("Backward spin on");
+			Thread.sleep(3000);
+			fixedMotor.triggerBackwardOff();
+			System.out.println("Backward spin off");
+		}
+		catch(InterruptedException ex){
+			Thread.currentThread().interrupt();
+		}
+
+	}*/
+}
---
+++ WitPi/src/Components/MotorPwm.java
@@ -0,0 +1,63 @@
+package Components;
+
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+//import com.pi4j.io.gpio.GpioController;
+//import com.pi4j.io.gpio.GpioFactory;
+//import com.pi4j.io.gpio.GpioPinDigitalOutput;
+
+public class MotorPwm extends MotorFixed{
+	private final GpioPinPwmOutput pwmPin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01);
+
+	public MotorPwm(Pin trigPinForward, Pin trigPinBackward){
+		super(trigPinForward, trigPinBackward);
+		pwmPin.setPwm(0);
+		this.trigPinForward.low();
+		this.trigPinBackward.low();
+	}
+	
+	public void setPower(int x){
+		if(x >= 0  && x <=1024){	
+		pwmPin.setPwm(x);
+		}
+		else{
+			System.err.println("Tried to set the power of Pwm motor to a value out of range.");
+		}
+	}
+	
+	public int getPower(){
+		return pwmPin.getPwm();
+	}
+	
+	/*public static void main( String[] args ) {
+		try{
+			System.out.println("Hello");
+			//Motor1
+			Pin forw1 = RaspiPin.GPIO_07;
+			Pin back1 = RaspiPin.GPIO_05;
+			MotorPwm motor = new MotorPwm(forw, back);
+			
+			motor.triggerForwardOn();
+			motor.setPower(1024);
+			System.out.println("Forward with power = "+motor.getPower());
+			Thread.sleep(3000);
+			motor.setPower(800);
+			System.out.println("Forward with power = "+motor.getPower());
+			Thread.sleep(3000);
+			motor.triggerForwardOff();
+			System.out.println("Backward with power = "+motor.getPower());
+			motor.triggerBackwardOn();
+			Thread.sleep(3000);
+			System.out.println("Backward with power = "+motor.getPower());
+			motor.setPower(1200);
+			Thread.sleep(3000);
+			motor.setPower(0);
+			motor.triggerForwardOff();
+			System.out.println("Off with power = "+motor.getPower());
+		}
+		catch(InterruptedException ex){
+			Thread.currentThread().interrupt();
+		}
+	}*/
+}
